2e55c95d6956d7c44130b9c4431314632286f6db
Shiyo Kon <14045299+shiyo-kon@user.noreply.gitee.com>
2024-10-29 14:55:24 +0800

    initialization

---
+++ src/main/java/com/sduonline/webdesign/WebDesignApplication.java
@@ -0,0 +1,13 @@
+package com.sduonline.webdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDesignApplication.class, args);
+    }
+
+}
